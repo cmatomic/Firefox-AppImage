612c5095bb365aa44c7a38362471f5d7fe825fd6
cmatomic <33186553+cmatomic@users.noreply.github.com>
2020-10-01 14:42:43 +0100

    Add files via upload

---
+++ codecs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+apt-get download libavcodec-ffmpeg-extra*
+apt-get download libavutil-ffmpeg*
+apt-get download libswresample-ffmpeg1
+
+for f in `ls -1 *.deb | sed 's/\(.*\)\..*/\1/'`
+do
+  dpkg-deb -x $f.deb $f
+  dpkg-deb -e $f.deb $f/DEBIAN
+done
+
+find ./ -name 'libs*-*.so*'  | xargs cp -t .
+find ./ -name 'libav*-*.so*'  | xargs cp -t .
+
